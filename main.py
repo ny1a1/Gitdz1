6603daac25b0a2d2696349d8cc03603387c9fa68
NY creator <n.yakush@istu.edu.ua>
2024-01-17 17:14:11 +0200

    first commit

---
+++ main.py
@@ -0,0 +1,21 @@
+import random
+import time
+for i in range(random.randint(1,15)):
+    a = random.randint(1,10)
+    if a == 10:
+        print("Hello World!")
+    elif a == 9:
+        print("Hello Jhon!")
+    elif a == 8:
+        print("Hi James!")
+    elif a == 5:
+        print("Git please work!")
+        print("Hello, it's United States of America!")
+    elif a == 2:
+        print("Hello Git!")
+    else:
+        print("Bay!")
+    time.sleep(1)
+print("Text")
+print("Та что за бред")
+print("HHHHHHH")
